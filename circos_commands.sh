3936568e54f9b5cdcf893709efd76965d1b72212
Linnéa Smeds <lbs5874@psu.edu>
2024-12-20 11:12:17 -0500

    Code for generating the circos plots

--- circos_commands.sh
+++ circos_commands.sh
@@ -1,5 +1,7 @@
-# CODE TO CALCULATE THE AMOUNT OF NEW SEQUENCE IN THE GENOME
+# CODE TO GENERATE FILES FOR CIRCOS PLOTS 
+# example of config files and data files are found under circos/
 
+#Start with human
 mkdir -p circos/human/
 
 # Create karyotype file
@@ -19,43 +21,27 @@ done
 cd circos/human/
 ~/software/circos-0.69-9/bin/circos -conf circos_halfInnerCircle.conf
 
-awk '(NR>1){if($4~/^hor/){if($4~/L)$/){print $1,$2,$3,"active_asat"}else{print $1,$2,$3,"active_asat"}}}else{split($4,s,"_"); print $1,$2,$3,s[1]} }' ref/repeats/chm13v2.0_censat_v2.0.bed |less
-
-
-
-################################################################################
-### BONOBO
-mkdir -p bonobo_pri/data
-grep -v random ref/mPanPan1.pri.cur.20231122.fasta.fai | awk '{gsub(/chr/,""); ; split($1,s,"_"); print "chr - pp"s[1],s[1],"0",$2,"vvlgrey"}'  >circos/bonobo_pri/karyotype.txt
-
-# With centromeres highlighted
-cut -f1  ref/mPanPan1.pri.cur.20231122.fasta.fai |grep -v random |grep -f - ref/centromeres/mPanPan1_v2.0.GenomeFeature_v1.0.bed | awk '(/Cen/){gsub(/chr/,""); split($1,s,"_"); print "pp"s[1],$2,$3}' >>circos/bonobo_pri/data/highlight_CEN.txt
-
-for nb in "APR" "DR" "GQ" "IR" "MR" "STR" "Z"
-do
-  cat densities/bonobo_pri/*_${nb}_100kb.bed |sed '/^[[:space:]]*$/d' |awk '{gsub(/chr/, "pp"); split($1,s,"_"); $3=$3-1; print s[1],$2,$3,$4}' >circos/bonobo_pri/data/${nb}.100kb.txt
-done
 
 ################################################################################
 ### ALL OTHER SPECIES
-#Naming chromosomes "nn", doesn't matter if all sp have the sama names
-for hap in "alt" #"pri" #
+#Naming chromosomes "nn", doesn't matter if all sp have the same names
+for hap in "alt" "pri" 
 do
-  cat T2T_primate_nonB/helpfiles/${hap}_species_list.txt |grep "siamang" |grep -v "human" |while read -r sp latin filename;
+  cat T2T_primate_nonB/helpfiles/${hap}_species_list.txt |grep -v "human" |while read -r sp latin filename;
   do
-#    cp -r circos/template circos/${sp}_${hap}/
-#    grep -v random ref/$filename.fai | awk '{gsub(/chr/,""); ; split($1,s,"_"); print "chr - nn"s[1],s[1],"0",$2,"vvlgrey"}'  >circos/${sp}_${hap}/karyotype.txt
+    cp -r circos/template circos/${sp}_${hap}/
+    grep -v random ref/$filename.fai | awk '{gsub(/chr/,""); ; split($1,s,"_"); print "chr - nn"s[1],s[1],"0",$2,"vvlgrey"}'  >circos/${sp}_${hap}/karyotype.txt
     for nb in "APR" "DR" "GQ" "IR" "MR" "STR" "Z"
     do
       cat densities/${sp}_${hap}/*_${nb}_100kb.bed |sed '/^[[:space:]]*$/d' |awk '{gsub(/chr/, "nn"); split($1,s,"_"); $3=$3-1; print s[1],$2,$3,$4}' >circos/${sp}_${hap}/data/${nb}.100kb.txt
     done
-#    spshort=`echo $filename |cut -f1 -d"."`
-#    cut -f1  ref/$filename.fai |grep -v random |grep -f - ref/centromeres/$spshort*GenomeFeature*.bed | awk '(/Cen/){gsub(/chr/,""); split($1,s,"_"); print "nn"s[1],$2,$3}' >circos/${sp}_${hap}/data/highlight_CEN.txt
+    spshort=`echo $filename |cut -f1 -d"."`
+    cut -f1  ref/$filename.fai |grep -v random |grep -f - ref/centromeres/$spshort*GenomeFeature*.bed | awk '(/Cen/){gsub(/chr/,""); split($1,s,"_"); print "nn"s[1],$2,$3}' >circos/${sp}_${hap}/data/highlight_CEN.txt
   done
 done
 
 # Run Circos for each of them
-for hap in "alt" #"pri" #
+for hap in "alt" "pri" 
 do
   cat T2T_primate_nonB/helpfiles/${hap}_species_list.txt |grep -v "human" |while read -r sp latin filename;
   do
