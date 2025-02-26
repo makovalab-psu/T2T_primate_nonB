7d0f9ba8fa6d75d97df13ae612dcd9a5a7c4a806
Linnéa Smeds <lbs5874@psu.edu>
2025-02-26 13:18:09 -0500

    adding code to check significance

--- functional_commands.sh
+++ functional_commands.sh
@@ -7,6 +7,8 @@
 # https://www.biorxiv.org/content/10.1101/2024.11.05.621973v1.full
 # Code used for generating the annotation files are found in 
 # https://github.com/makovalab-psu/GreatApeT2T-G4s/tree/main/datasets/dataForAnalysis/geneAnnotations
+# And the actual files can be downloaded from: 
+# https://github.com/makovalab-psu/GreatApeT2T-G4s/tree/main/datasets/functionalOutputs/Homo_sapiens
 
 # Abbreviations for functional classes and file names used inhouse our found in 
 # T2T_primate_nonB/helpfiles/functional_classes.txt 
@@ -20,6 +22,14 @@ do
     awk '($1=="chrY"){print}' $func_path/$file >ref/annotation/human/chrY.$file
 done 
 
+# Check size of annotated regions 
+cat T2T_primate_nonB/helpfiles/functional_classes.txt |while read -r class bedfile
+do 
+    len1=`awk '{sum+=$3-$2}END{print sum}' ref/annotation/human/$bedfile`
+    len2=`awk '{sum+=$3-$2}END{print sum}' <(mergeBed -i ref/annotation/human/$bedfile)`
+
+    echo $class":" $len1" "$len2
+done 
 
 # INTERSECT FUNCTIONAL ANNOTATION WITH NON-B
 cat T2T_primate_nonB/helpfiles/pri_species_list.txt |grep "human" |while read -r sp latin filename;
@@ -31,11 +41,12 @@ do
     do 
         echo "Looking at $class" 
         tmp="genome "$class
-        len=`awk '"'"'{sum+=$3-$2}END{print sum}'"'"' ref/annotation/'$sp'/$bedfile`
+        len=`mergeBed -i ref/annotation/'$sp'/$bedfile |awk '"'"'{sum+=$3-$2}END{print sum}'"'"'`
+        echo "Length of $class is $len"
         cat densities/'${sp}'_pri_nonB_genome_wide.txt |grep -v "all" | while read -r non_b tot dens;
         do
             echo "looking at $non_b"
-            d=`intersectBed -a <(cut -f1-4 ref/annotation/human/$bedfile) -b <(cat nonB_annotation/'${sp}'_pri/genome_${non_b}.bed) -wo |awk -v l=$len -v dtot=$dens '"'"'{sum+=$8}END{d=sum/l; frac=d/dtot; print frac}'"'"'`
+            d=`intersectBed -a <(mergeBed -i ref/annotation/human/$bedfile) -b <(cat nonB_annotation/'${sp}'_pri/genome_${non_b}.bed) -wo |awk -v l=$len -v dtot=$dens '"'"'{sum+=$7}END{d=sum/l; frac=d/dtot; print frac}'"'"'`
             tmp=`echo $tmp" "$d`
             echo $tmp >tmp
         done
@@ -43,6 +54,111 @@ do
     done
     ' | sbatch -J $sp --ntasks=1 --cpus-per-task=1 --mem-per-cpu=8G --time=5:00:00 --partition=open
 done
+
+# Plot with R/plot_fig5_functional_enrichment.R
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CHECK SIGNIFICANCE OF THE BARS IN FIGURE 5. 
+
+# Subsample the functional regions to get a distribution of the enrichment
+# values. If distribution overlaps with 1 (genome average), we deem this 
+# comparison not to be significally different from the genome average.
+
+# Subsample X sets 
+sp="human"
+mkdir -p functional/$sp/resample/
+perc=50
+
+cat T2T_primate_nonB/helpfiles/functional_classes.txt |while read -r class bedfile
+do 
+    num=`awk -v p=$perc '{}END{num=int(NR*(1/p)); print num}' ref/annotation/$sp/$bedfile`
+    # Subsample the sequences 10 times 
+    echo '#!/bin/bash
+    module load bedtools/2.31.0
+    for i in {11..20}
+    do 
+        shuf -n '$num' ref/annotation/'$sp'/'$bedfile' |sort -k1,1 -k2,2n |mergeBed -i -  >functional/'$sp'/resample/Resamp.'$perc'perc.'$class'.$i.bed
+    done
+    ' |sbatch -J $class -o slurm-"$perc"perc-$class.%j.out --requeue --ntasks=1 --mem-per-cpu=1G --cpus-per-task=1 --time=1:00:00 --partition=open
+done 
+
+# Calculate the enrichment for each of the subsamples
+sp="human"
+for subset in "50perc" # "10perc" # 
+do
+    cat T2T_primate_nonB/helpfiles/functional_classes.txt |while read -r class bedfile
+    do 
+        echo '#!/bin/bash
+        module load bedtools/2.31.0
+        for i in {11..20}
+        do 
+            echo "Class NonB Enrichment" |sed "s/ /\t/g" >functional/'$sp'/resample/'$subset'.'$class'.$i.summary.txt
+            len=`awk '"'"'{sum+=$3-$2}END{print sum}'"'"' functional/'$sp'/resample/Resamp.'$subset'.'$class'.$i.bed`
+            cat densities/'${sp}'_pri_nonB_genome_wide.txt |grep -v "all" | while read -r non_b tot dens;
+            do
+                d=`intersectBed -a functional/'$sp'/resample/Resamp.'$subset'.'$class'.$i.bed -b <(cat nonB_annotation/'${sp}'_pri/genome_${non_b}.bed) -wo |awk -v l=$len -v dtot=$dens '"'"'{sum+=$7}END{d=sum/l; frac=d/dtot; print frac}'"'"'`
+                echo '$class' $non_b $d >>functional/'$sp'/resample/'$subset'.'$class'.$i.summary.txt
+            done
+        done 
+        ' |sbatch -J $class.$subset -o slurm-$subset-$class.%j.out --requeue --ntasks=1 --cpus-per-task=1 --mem-per-cpu=4G --time=1:00:00 --partition=open
+    done 
+done
+
+# Merge the results
+# This is done separately from above since the jobs are run in parallell
+sp="human"
+rep=20
+for subset in "50perc" "10perc" #
+do
+    echo "Rep Class NonB Enrichment" |sed "s/ /\t/g" >functional/$sp/$subset.${rep}rep.summary.txt
+    cat T2T_primate_nonB/helpfiles/functional_classes.txt |while read -r class bedfile
+    do  
+        for i in $( seq 1 $rep )
+        do 
+         awk '(NR>1){print "'$i'",$0}' functional/$sp/resample/$subset.$class.$i.summary.txt |sed "s/ /\t/g" >>functional/$sp/$subset.${rep}rep.summary.txt
+       done 
+    done 
+done
+
+# Get the min and the max values for each class and non-B type
+sp="human"
+rep=20
+for subset in "10perc" "50perc" #
+do
+    echo "Class NonB Min Max" |sed "s/ /\t/g" >functional/$sp/$subset.${rep}rep.minmax.txt
+    cat T2T_primate_nonB/helpfiles/functional_classes.txt |while read -r class bedfile
+    do 
+        for nonb in APR DR GQ IR MR TRI STR Z
+        do 
+            min=`grep $class functional/$sp/$subset.${rep}rep.summary.txt |awk -v nb=$nonb -v OFS="\t" '($3==nb){print $4}' |sort -n |head -n1`
+            max=`grep $class functional/$sp/$subset.${rep}rep.summary.txt |awk -v nb=$nonb -v OFS="\t" '($3==nb){print $4}' |sort -n |tail -n1`
+            echo $class $nonb $min $max |sed "s/ /\t/g" >>functional/$sp/$subset.${rep}rep.minmax.txt
+        done 
+    done 
+done
+
+# For the 100 runs, we actually want to remove the ~5% most extreme (meaning
+# we remove the top and bottom two values, saving a 96% CI)
+sp="human"
+rep=100
+for subset in "10perc"
+do
+    echo "Class NonB Min Max" |sed "s/ /\t/g" >functional/$sp/$subset.${rep}rep.96CI.txt
+    cat T2T_primate_nonB/helpfiles/functional_classes.txt |while read -r class bedfile
+    do 
+        for nonb in APR DR GQ IR MR TRI STR Z
+        do 
+            min=`grep $class functional/$sp/$subset.${rep}rep.summary.txt |awk -v nb=$nonb -v OFS="\t" '($3==nb){print $4}' |sort -n |head -n3 |tail -n1`
+            max=`grep $class functional/$sp/$subset.${rep}rep.summary.txt |awk -v nb=$nonb -v OFS="\t" '($3==nb){print $4}' |sort -n |tail -n3 |head -n1`
+            echo $class $nonb $min $max |sed "s/ /\t/g" >>functional/$sp/$subset.${rep}rep.96CI.txt
+        done 
+    done 
+done
+
+
+# Plot with R/plot_fig10_functional_enrichment.R
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # CORRECT FOR GC CONTENT 
@@ -58,7 +174,7 @@ do
         echo '#!/bin/bash
         module load bedtools/2.31.0
         newfile=`echo '$bedfile' |sed "s/.bed/.withGC.bed/"`
-        bedtools nuc -fi ref/assemblies/'$filename' -bed <(cut -f1-3 ref/annotation/'$sp'/'$bedfile') >ref/annotation/'$sp'/$newfile
+        bedtools nuc -fi ref/assemblies/'$filename' -bed <(cut -f1-3 ref/annotation/'$sp'/'$bedfile' |mergeBed -i -) >ref/annotation/'$sp'/$newfile
         '| sbatch -J $class --ntasks=1 --cpus-per-task=1 --time=1:00:00 --partition=open
     done 
 done 
@@ -80,7 +196,7 @@ do
     ' | sbatch --ntasks=1 --cpus-per-task=1 --time=1:00:00 --partition=open
 done 
 
-# Then go through the enrichment file and correct the GQ value 
+# Then go through the enrichment file and correct the GQ value (GQ is in the 5th column)
 cat T2T_primate_nonB/helpfiles/pri_species_list.txt |grep "human" |while read -r sp latin filename;
 do
     gc_genome=`awk  '{gc=$1; tot=$1+$2}END{gccont=gc/tot; print gccont}' ref/assemblies/$filename.nuc`
@@ -108,5 +224,111 @@ do
          done
     done
 done
-# There are no clear relationships with GC for the other non-B types! 
+# There are no clear relationships with GC for the non-B types other than G4!
 
+# Make a supplementary figure where only G4 is corrected for GC content
+# Plot with R/plot_figS11_enrichment_functional_GCcorrected.R
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Exploring enhancers and promoters, are there overlaps in the annotation?
+
+# Number of promoters 
+ wc ref/annotation/human/promoter_protCode_singleTrans.bed
+# 19578 117468 851593 ref/annotation/human/promoter_protCode_singleTrans.bed
+# Total length of promoters (mean length should be close to 1000 by def)
+awk '{sum+=$3-$2}END{print sum}' ref/annotation/human/promoter_protCode_singleTrans.bed
+# 19234458
+# Mean length (slightly shorter than 1000)
+awk '{sum+=$3-$2}END{print sum/NR}' ref/annotation/human/promoter_protCode_singleTrans.bed
+982.453
+
+# Number of enhancers
+wc ref/annotation/human/enhancer_protCode_singleTrans.bed 
+# 106998  641988 5894744 ref/annotation/human/enhancer_protCode_singleTrans.bed
+#Total length of enhancers
+awk '{sum+=$3-$2}END{print sum}' ref/annotation/human/enhancer_protCode_singleTrans.bed 
+# 55359339
+# Mean length 
+awk '{sum+=$3-$2}END{print sum/NR}' ref/annotation/human/enhancer_protCode_singleTrans.bed
+# 517
+
+ 
+#Intersect: 
+intersectBed -a ref/annotation/human/enhancer_protCode_singleTrans.bed -b ref/annotation/human/promoter_protCode_singleTrans.bed -wo |wc
+# 11261  146393 1154808
+# Size of intersection
+
+intersectBed -a ref/annotation/human/enhancer_protCode_singleTrans.bed -b ref/annotation/human/promoter_protCode_singleTrans.bed |awk '{sum+=$3-$2}END{print sum}' 
+# 3075721
+
+# Not so large proportion of enhancers overlapping with promoters.
+
+# Check within each of these categories how much overlap there is between GQ and Z-DNA. 
+# First create bed files of each set of regions
+intersectBed -a ref/annotation/human/promoter_protCode_singleTrans.bed -b nonB_annotation/human_pri/genome_GQ.bed |cut -f1-3 >functional/human/overlap/test.promoter.GQ.bed
+intersectBed -a ref/annotation/human/promoter_protCode_singleTrans.bed -b nonB_annotation/human_pri/genome_Z.bed |cut -f1-3 >functional/human/overlap/test.promoter.Z.bed
+intersectBed -a ref/annotation/human/enhancer_protCode_singleTrans.bed -b nonB_annotation/human_pri/genome_GQ.bed |cut -f1-3 >functional/human/overlap/test.enhancer.GQ.bed
+intersectBed -a ref/annotation/human/enhancer_protCode_singleTrans.bed -b nonB_annotation/human_pri/genome_Z.bed |cut -f1-3 >functional/human/overlap/test.enhancer.Z.bed
+
+# Checking sizes of GQ and Z in promoters 
+awk '{sum+=$3-$2}END{print sum}' functional/human/overlap/test.promoter.GQ.bed
+# 1012270
+awk '{sum+=$3-$2}END{print sum}' functional/human/overlap/test.promoter.Z.bed 
+# 86799
+# Then intersect the regions - only a TINY fraction is overlapping 
+ intersectBed -a functional/human/overlap/test.promoter.GQ.bed -b functional/human/overlap/test.promoter.Z.bed |  awk '{sum+=$3-$2}END{print sum}' 
+# 1379
+
+# Checking sizes of GQ and Z in enhancers 
+awk '{sum+=$3-$2}END{print sum}' functional/human/overlap/test.enhancer.GQ.bed
+#  1876727
+awk '{sum+=$3-$2}END{print sum}' functional/human/overlap/test.enhancer.Z.bed
+#  203501 
+# Intersect the regions - even less, in terms of fraction.
+intersectBed -a functional/human/overlap/test.enhancer.GQ.bed -b functional/human/overlap/test.enhancer.Z.bed |  awk '{sum+=$3-$2}END{print sum}'
+# 1545
+
+# Checking number of enhancers and promoters that has both GQ and Z-DNA 
+# (even if not overlapping)
+paste functional/human/overlap/promoter_protCode_singleTrans.GQ.bed functional/human/overlap/promoter_protCode_singleTrans.Z.bed |awk '($5>0 && $11>0){print}' |wc
+# 2805   33660  271785
+paste functional/human/overlap/enhancer_protCode_singleTrans.GQ.bed functional/human/overlap/enhancer_protCode_singleTrans.Z.bed |awk '($5>0 && $11>0){print}' |wc
+# 4360   52320  494837
+# This corresponds to 4.1% and 14.4% respectively
+
+
+# Checking number of promoters that has both GQ and Z DNA annotated for the same bp
+intersectBed -a functional/human/overlap/test.promoter.GQ.bed -b functional/human/overlap/test.promoter.Z.bed | uniq |intersectBed -a ref/annotation/human/promoter_protCode_singleTrans.bed -b - |cut -f4 |uniq |wc -l
+# 205
+# And enhancers 
+intersectBed -a functional/human/overlap/test.enhancer.GQ.bed -b functional/human/overlap/test.enhancer.Z.bed | uniq |intersectBed -a ref/annotation/human/enhancer_protCode_singleTrans.bed -b - |cut -f4 |uniq |wc -l
+# 221
+# This is 1.0% and 0.2% of the total number of promoters and enhancers respectively.
+
+# Number of overlapping bp between GQ and Z-DNA in promoters and enhancers
+intersectBed -a functional/human/overlap/test.promoter.GQ.bed -b functional/human/overlap/test.promoter.Z.bed |uniq | awk '{sum+=$3-$2}END{print sum}'
+#1152
+intersectBed -a functional/human/overlap/test.enhancer.GQ.bed -b functional/human/overlap/test.enhancer.Z.bed |uniq | awk '{sum+=$3-$2}END{print sum}'
+#1382
+# We want to check how much this is in terms of GQ and Z-DNA regions annotated, 
+# dividing on the number given above:
+# 1152/1012270=0.0011 Of GQ in promoters 
+# 1382/1876727=0.0007 Of GQ in enhancers 
+# 1152/86799=0.0133 Of Z in promoters
+# 1382/203501=0.0068 Of Z in enhancers
+
+# As we also mentioned Origins of replication in the paper, we should also check
+# how much overlap there is with these.
+intersectBed -a ref/annotation/human/core_stochastic_origins_chm13v2.0_hglft.ucsc.bed  -b nonB_annotation/human_pri/genome_GQ.bed |cut -f1-3 >functional/human/overlap/test.origin.GQ.bed
+intersectBed -a ref/annotation/human/core_stochastic_origins_chm13v2.0_hglft.ucsc.bed  -b nonB_annotation/human_pri/genome_Z.bed |cut -f1-3 >functional/human/overlap/test.origin.Z.bed
+# Checking sizes of GQ and Z in origins of replication 
+awk '{sum+=$3-$2}END{print sum}' functional/human/overlap/test.origin.GQ.bed
+#  4212032
+awk '{sum+=$3-$2}END{print sum}' functional/human/overlap/test.origin.Z.bed
+#  649601
+# Intersect the regions:
+intersectBed -a functional/human/overlap/test.origin.GQ.bed -b functional/human/overlap/test.origin.Z.bed |  awk '{sum+=$3-$2}END{print sum}'
+# 6422
+# Almost nothing in terms of fraction! 
